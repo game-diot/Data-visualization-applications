0a457e896f00151c8f4845cd53b8969e05555607
张泽洁 <3296592663@qq.com>
2026-02-06 16:29:33 +0800

    React记录
    前端工程基础化模块开始

--- react_frontend/DataV/src/main.tsx
+++ react_frontend/DataV/src/main.tsx
@@ -1,16 +1,2 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ErrorBoundary } from './app/components/ErrorBoundary';
-import { App } from './app/App';
-import './shared/i18n';
-import './styles/reset.css';
-import './styles/variables.css';
-import './styles/globals.css';
-
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<ErrorBoundary>
-			<App />
-		</ErrorBoundary>
-	</StrictMode>,
-);
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
