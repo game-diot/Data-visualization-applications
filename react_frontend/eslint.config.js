9f9b4aca6b4d36ec9cd46039ecb30e8dc40597ce
张泽洁 <3296592663@qq.com>
2026-02-08 21:05:17 +0800

    chore: fix nested git repo and restructure project

--- react_frontend/eslint.config.js
+++ react_frontend/eslint.config.js
@@ -3,21 +3,38 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettier,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // Prettier 集成
+      'prettier/prettier': 'error',
+      // 自动排序 Import
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      // 禁止 dangerous any (允许显式忽略)
+      '@typescript-eslint/no-explicit-any': 'warn',
+      // 禁止未使用变量 (忽略 _ 开头的)
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
   },
-])
+  eslintConfigPrettier, // 关闭冲突规则
+)
--- react_frontend/src/vite-env.d.ts
+++ react_frontend/src/vite-env.d.ts
@@ -1,9 +1,8 @@
-interface ImportMetaEnv{
-    readonly VITE_API_BASE_URL:string;
-    readonly VITE_APP_TITLE:string;
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_APP_TITLE: string
 }
 
-
-interface ImportMeta{
-    readonly  env:ImportMetaEnv
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
